d30838a7f8790d9df171a1cf96c223df9edd2a02
molchalih <26502678+molchalih@users.noreply.github.com>
2026-04-19 19:12:36 +0200

    chore: add passWithNoTests to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
+  passWithNoTests: true,
   moduleNameMapper: {
     "^@raycast/api$": "<rootDir>/src/__mocks__/@raycast/api.ts",
   },
